603cc03495b5b9da9b51c369d3f4e7d770e62955
Ksenx73 <hello@itcampe.com>
2021-10-21 16:11:12 +0300

    first commit

---
+++ src/it/campe/Main.java
@@ -0,0 +1,15 @@
+package it.campe;
+
+public class Main {
+
+    public static int[] array = new int[]{-1, 2, 3, -4, -5};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(array[0]);
+        System.out.println(array[1]);
+        System.out.println(array[2]);
+        System.out.println(array[3]);
+        System.out.println(array[4]);
+    }
+}
